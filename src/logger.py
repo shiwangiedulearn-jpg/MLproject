6bafa5ad1adc459677aac2fe83a1b98b6943c706
shiwangiedulearn-jpg <shiwangi.edulearn@gmail.com>
2026-01-31 19:18:09 +0530

    logging and exception

--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,18 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
 
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     level=logging.INFO,
-    format="[%(asctime)s ] %(linenoo)d %(name)s - (levelname)s - %(message)s",
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
 )
+
+
+logger = logging.getLogger(__name__)
